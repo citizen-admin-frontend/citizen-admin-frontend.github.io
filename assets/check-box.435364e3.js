6fd61c007c1e1f678a6e3cb1224b799a377b96a3
Brayan Jenzy La Cruz Carreño <blacruzc@emeal.nttdata.com>
2022-11-21 20:10:07 -0500

    v4

--- assets/check-box.435364e3.js
+++ assets/check-box.435364e3.js
@@ -1,4 +1,4 @@
-import{c8 as l,ah as u,ai as n,aj as o}from"./index.0811e576.js";const i=Object.freeze(Object.defineProperty({__proto__:null,default:l},Symbol.toStringTag,{value:"Module"}));var t={};const d=u(i);/*!
+import{c8 as l,ah as u,ai as n,aj as o}from"./index.ea610c3a.js";const i=Object.freeze(Object.defineProperty({__proto__:null,default:l},Symbol.toStringTag,{value:"Module"}));var t={};const d=u(i);/*!
  * devextreme-vue
  * Version: 22.1.5
  * Build date: Fri Sep 02 2022
